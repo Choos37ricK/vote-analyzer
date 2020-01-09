0da4a0293909853384f1b65e1e7ee2fa7f938235
Girin Ilya <39518953+Choos37ricK@users.noreply.github.com>
2020-01-09 12:18:43 +0300

    project added

---
+++ src/WorkTime.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+import java.util.TreeSet;
+
+public class WorkTime
+{
+    private TreeSet<TimePeriod> periods;
+
+    /**
+     * Set of TimePeriod objects
+     */
+    public WorkTime()
+    {
+        periods = new TreeSet<>();
+    }
+
+    public void addVisitTime(long visitTime)
+    {
+        Date visit = new Date(visitTime);
+        TimePeriod newPeriod = new TimePeriod(visit, visit);
+        for(TimePeriod period : periods)
+        {
+            if(period.compareTo(newPeriod) == 0)
+            {
+                period.appendTime(visit);
+                return;
+            }
+        }
+        periods.add(new TimePeriod(visit, visit));
+    }
+
+    public String toString()
+    {
+        //String line = "";
+        StringBuilder builder = new StringBuilder();
+        for(TimePeriod period : periods)
+        {
+            if(!builder.toString().isEmpty()) {
+                builder.append(", ");
+                //line += ", ";
+            }
+            builder.append(period);
+            //line += period;
+        }
+        return builder.toString();
+    }
+}
---
+++ src/XMLHandler.java
@@ -0,0 +1,68 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class XMLHandler extends DefaultHandler {
+
+    private static SimpleDateFormat birthDayFormat = new SimpleDateFormat("yyyy.MM.dd");
+    private static SimpleDateFormat visitDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+    private Voter voter;
+    private static HashMap<Integer, WorkTime> voteStationWorkTimes = new HashMap<>();
+    private static HashMap<Voter, Integer> voterCounts = new HashMap<>();
+
+    public XMLHandler() {
+        voterCounts = new HashMap<>();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        try {
+            if (qName.equals("voter") && voter == null) {
+                Date birthDay = birthDayFormat.parse(attributes.getValue("birthDay"));
+                voter = new Voter(attributes.getValue("name"), birthDay);
+            } else if (qName.equals("visit") && voter != null) {
+                int count = voterCounts.getOrDefault(voter, 0);
+                voterCounts.put(voter, count + 1);
+
+                int station = Integer.parseInt(attributes.getValue("station"));
+                Date time = visitDateFormat.parse(attributes.getValue("time"));
+                WorkTime workTime = voteStationWorkTimes.get(station);
+                if(workTime == null)
+                {
+                    workTime = new WorkTime();
+                    voteStationWorkTimes.put(station, workTime);
+                }
+                workTime.addVisitTime(time.getTime());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (qName.equals("voter")) {
+            voter = null;
+        }
+    }
+
+    public void printVotingResultsSAX() {
+        System.out.println("Voting station work times: ");
+        for (Integer votingStation : voteStationWorkTimes.keySet()) {
+            WorkTime workTime = voteStationWorkTimes.get(votingStation);
+            System.out.println("\t" + votingStation + " - " + workTime);
+        }
+
+        System.out.println("\nDuplicated voters: ");
+        for (Voter voter : voterCounts.keySet()) {
+            int count = voterCounts.get(voter);
+            if (count > 1) {
+                System.out.println("\t" + voter.toString() + " - " + count);
+            }
+        }
+    }
+}
